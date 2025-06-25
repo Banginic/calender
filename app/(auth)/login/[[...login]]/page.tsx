1de619d1c2e6e10f9716182b38da6da1fd92f5e8
Binary Digitz <borisndoh@gmail.com>
2025-06-25 14:18:05 +0100

    Add Login and Sign up clerk functionality

--- app/(auth)/login/[[...login]]/page.tsx
+++ app/(auth)/login/[[...login]]/page.tsx
@@ -1,9 +1,19 @@
-import React from 'react'
+import { SignIn } from "@clerk/nextjs"
+import Image from "next/image"
+
 
 function Login() {
   return (
-    <main>
-      login
+    <main className='flex flex-col items-center p-4 gap-8 animate-fade-in'>
+      <Image 
+      src={'/assets/logo.svg'}
+      width={100}
+      height={100}
+      alt="logo"
+      />
+      <div className="mt-4">
+        <SignIn />
+      </div>
     </main>
   )
 }
--- app/(auth)/register/[[...register]]/page.tsx
+++ app/(auth)/register/[[...register]]/page.tsx
@@ -1,10 +1,20 @@
-import React from 'react'
+import { SignUp } from "@clerk/nextjs"
+import Image from "next/image"
+
 
 function Register() {
   return (
-    <div>
-      register
-    </div>
+    <main className='flex flex-col items-center p-4 gap-8 animate-fade-in'>
+      <Image 
+      src={'/assets/logo.svg'}
+      width={100}
+      height={100}
+      alt="logo"
+      />
+      <div className="mt-4">
+        <SignUp />
+      </div>
+    </main>
   )
 }
 
